de8c4e5fe81fa5f31abc96497dac03e9c4df5528
44davii <112115893+44davii@users.noreply.github.com>
2024-05-19 13:00:00 -0300

    Update login.py

--- tkinter/login.py
+++ tkinter/login.py
@@ -1,12 +1,12 @@
 from customtkinter import*
 
-maicou = CTk()
-maicou.geometry("500x400")
+tela = CTk()
+tela.geometry("500x400")
 
-title = CTkLabel(maicou, text="Login", font=("Arial", 20)).pack(pady=20)
-user = CTkEntry(maicou, placeholder_text="User name...", width=300).pack(pady=20)
-password = CTkEntry(maicou, placeholder_text="Password...", width=300, show="*").pack()
-CTkCheckBox = CTkCheckBox(maicou, text="Lembrar de mim").place(x=100, y=180)
-button = CTkButton(maicou, text="Login", width=300).pack(pady=70)
+title = CTkLabel(tela, text="Login", font=("Arial", 20)).pack(pady=20)
+user = CTkEntry(tela, placeholder_text="User name...", width=300).pack(pady=20)
+password = CTkEntry(tela, placeholder_text="Password...", width=300, show="*").pack()
+CTkCheckBox = CTkCheckBox(tela, text="Lembrar de mim").place(x=100, y=180)
+button = CTkButton(tela, text="Login", width=300).pack(pady=70)
 
-maicou.mainloop()
+tela.mainloop()
